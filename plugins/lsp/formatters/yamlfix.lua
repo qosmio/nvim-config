98388aae2c23d93782f13fc5a08a5d61f11f308a
Qosmio <datapronix@protonmail.com>
2023-02-19 18:16:06 -0500

    update

---
+++ plugins/lsp/formatters/yamlfix.lua
@@ -0,0 +1,35 @@
+local null_ls_status_ok, null_ls = pcall(require, "null-ls")
+if not null_ls_status_ok then
+  return
+end
+local h = require "null-ls.helpers"
+local methods = require "null-ls.methods"
+
+local FORMATTING = methods.internal.FORMATTING
+
+local formatter = {
+  name = "yamlfix",
+  meta = {
+    url = "https://github.com/lyz-code/yamlfix",
+    description = "A simple opinionated yaml formatter that keeps your comments!",
+  },
+  method = FORMATTING,
+  filetypes = {
+    "yaml",
+  },
+  generator = null_ls.formatter {
+    command = "yamlfix",
+    args = {
+      -- "--config-file",
+      -- vim.fn.stdpath "config" .. "/lua/custom/plugins/config/.yamlfix",
+      "-",
+    },
+    to_stdin = true,
+  },
+}
+-- h.make_builtin(formatter)
+local M = {}
+M.setup = function()
+  null_ls.register(formatter)
+end
+return M
---
+++ plugins/lsp/servers/ansiblels.lua
@@ -0,0 +1,29 @@
+-- npm install -g @ansible/ansible-language-server
+
+return {
+  filetypes = {
+       "yaml.ansible",
+  },
+  settings = {
+    ansible = {
+      ansible = {
+        path = "ansible",
+        useFullyQualifiedCollectionNames = true
+      },
+      ansibleLint = {
+        enabled = true,
+        path = "ansible-lint"
+      },
+      executionEnvironment = {
+        enabled = false
+      },
+      python = {
+        interpreterPath = "python"
+      },
+      completion = {
+          provideRedirectModules = true,
+          provideModuleOptionAliases = true
+      }
+    },
+  },
+}
