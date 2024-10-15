f031be2b813c0f62dd7a2584c9d7067782fd785e
Qosmio <datapronix@protonmail.com>
2024-10-15 14:35:11 -0400

    update

--- lua/plugins/config/conform.lua
+++ lua/plugins/config/conform.lua
@@ -43,9 +43,9 @@ M.config = {
   formatters_by_ft = {
     lua = { "stylua" },
     -- Conform will run multiple formatters sequentially
-    go = { "goimports", "gofumpt" },
+    -- go = { "goimports", "gofumpt" },
     -- Use a sub-list to run only the first available formatter
-    javascript = { { "prettierd", "prettier" } },
+    javascript = { "prettierd", "prettier", stop_after_first = true },
     -- You can use a function here to determine the formatters dynamically
     python = function(bufnr)
       if require("conform").get_formatter_info("ruff_format", bufnr).available then
